fd76e7e3fefe352d198fbc2c4d1c8c9f6e6c5866
beka <beka.badagadze@geolab.edu.ge>
2024-06-04 23:44:28 +0400

    nth

--- server.js
+++ server.js
@@ -1,43 +1,35 @@
 const express = require("express");
 const { Web3 } = require("web3");
-const axios = require("axios");
+// const axios = require("axios");
+// const WETH_abi = require("./rc20.json");
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 
+const address = "0xB5A094A53b6e9003645c12c48676Bb6089e2e302";
+
 const web3 = new Web3(new Web3.providers.HttpProvider(process.env.RPC_URL));
 const account = web3.eth.accounts.privateKeyToAccount(process.env.PRIVATE_KEY);
 web3.eth.accounts.wallet.add(account);
 
-// USDC contract address and ABI
-const USDC_CONTRACT_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606EB48"; // Mainnet USDC contract address
-const USDC_ABI = [
-  {
-    constant: true,
-    inputs: [{ name: "_owner", type: "address" }],
-    name: "balanceOf",
-    outputs: [{ name: "balance", type: "uint256" }],
-    type: "function",
-  },
-];
+// const WETH_CONTRACT_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 
-const usdcContract = new web3.eth.Contract(USDC_ABI, USDC_CONTRACT_ADDRESS);
+// const WETHContract = new web3.eth.Contract(WETH_abi, WETH_CONTRACT_ADDRESS);
 
 app.get("/", (_, res) => {
-  res.send("Hello connected via Infura!");
+  res.send("Hello");
 });
 
 app.get("/balance", async (req, res) => {
   try {
-    const address = "0xB5A094A53b6e9003645c12c48676Bb6089e2e302";
     const ethBalance = await web3.eth.getBalance(address);
-    const usdcBalance = await usdcContract.methods.balanceOf(address).call();
+    // const WETHBalance = await WETHContract.methods.balanceOf(address).call();
 
     res.send({
       address: address,
       ethBalance: web3.utils.fromWei(ethBalance, "ether"),
-      usdcBalance: web3.utils.fromWei(usdcBalance, "mwei"),
+      //   WETHBalance: web3.utils.fromWei(usdcBalance, "ether"),
     });
   } catch (error) {
     res.status(500).send({ error: error.message });
